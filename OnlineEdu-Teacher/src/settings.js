634e45ffcb52face87dc2b681a6bd8dd5e79afa9
yangyz <yangyz26650@yunrong.cn>
2024-05-26 17:39:26 +0800

    first commit

---
+++ OnlineEdu-Teacher/src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  title: '在线教育平台讲师端',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+
+}
---
+++ OnlineEdu/src/main/java/xyz/refrain/onlineedu/config/MinioConfig.java
@@ -0,0 +1,27 @@
+package xyz.refrain.onlineedu.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+//@PropertySource(factory = YamlAndPropertySourceFactory.class, value = {"classpath:application-dev.yaml"})
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.endpoint}")
+    private String url;
+    @Value("${minio.accessKey}")
+    private String accessKey;
+    @Value("${minio.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(url)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+}
